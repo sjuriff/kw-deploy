f22903d581c1e32fc7d22f637d9e4c63576da48d
sjuriff <sjursand@gmail.com>
2024-01-31 13:39:57 +0100

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "kartbaserte-websystemer",
+  base: "kw-deploy",
   plugins: [react()],
 })
