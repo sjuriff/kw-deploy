b2305c7bf24f91fa719e125decae95343258ddcf
sjuriff <sjursand@gmail.com>
2024-01-31 14:01:02 +0100

    ready to deploy 5

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "kw-deploy",
-  plugins: [react()],
-})
+export default {
+  base: "kw-deploy"
+}
